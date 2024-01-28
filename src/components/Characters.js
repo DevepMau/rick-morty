ebda78bdf0360712d677cbc75f4e8ed028f3139e
DevepMau <oshiettecs@gmail.com>
2024-01-28 17:16:55 -0300

    Extraer lista de nombres de la api

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,7 +1,19 @@
 
 export default function Characters(props) {
-  console.log(props);
-  return (
-    <h1>Desde characters.js</h1>
-  )
+
+    const { characters } = props;
+
+    return (
+        <div className="character">
+            <h1>Personajes</h1>
+            <span className="back-home">Volver a la home</span>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <p>{character.name}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
